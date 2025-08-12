6b176b25b9dd176e30bb2d39482f342ba1623803
murilomunari <munarimurilo441@gmail.com>
2025-08-12 10:24:52 -0300

    First commit

---
+++ src/main/java/com/JShort/JShortApplication.java
@@ -0,0 +1,13 @@
+package com.JShort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JShortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JShortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JShort/JShortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JShort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JShortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
